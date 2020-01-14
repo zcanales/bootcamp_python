d7f1a6f4621436c1f0bca3903220292f4323dd5f
acoudouy <acoudouy@student.42.fr>
2020-01-14 14:45:41 +0100

    ex02 en cours

--- day01/ex02/test.py
+++ day01/ex02/test.py
@@ -1,3 +1,73 @@
 from vector import Vector
 
+print("Vector 1")
+v1 = Vector([0.0,5.0,10.0,20.0])
+print(v1)
+print("")
 
+print("Vector 2")
+v2 = Vector(5)
+print(v2)
+print("")
+
+
+print("Vector 3")
+v3 = Vector((3,8))
+print(v3)
+print("")
+
+
+print("Vector 4")
+v4 = Vector(1)
+print(v4)
+print("")
+
+
+print("Vector 5")
+v5 = Vector("lol")
+print(v5)
+print("")
+
+print("Add : v1 + 1")
+v6 = v1 + 1
+print(v6)
+print(v1)
+print("")
+
+print("Add : 1 + v1")
+v6 = 1 + v1
+print(v6)
+print(v1)
+print("")
+
+print("Add : v3 + v2")
+v9 = v3 + v2
+print(v9)
+print(v3)
+print(v2)
+print("")
+
+print("Sub : v3 - 2")
+v7 = v3 - 2
+print(v7)
+print(v3)
+print("")
+
+print("Sub : 2 - v3")
+v7 = 2 - v3
+print(v7)
+print(v3)
+print("")
+
+print("Sub : v3 - v2")
+v9 = v3 - v2
+print(v9)
+print(v3)
+print(v2)
+print("")
+
+print("Truediv : v2 / 2")
+v8 = v2 / 2
+print(v8)
+print(v2)
+print("")
--- day01/ex02/vector.py
+++ day01/ex02/vector.py
@@ -1,6 +1,134 @@
 class Vector:
     def __init__(self, var):
-        if isinstance(var, list) == 1
-            for item in var
-                if isinstance(item, float) == 0
+        i = 0
+        self.data = []
+        if isinstance(var, list) == 1:
+            for item in var:
+                if isinstance(item, float) == 0:
+                    print("Your list is not only composed by float")
+                    return
+            self.data = var
+        elif isinstance(var, int) == 1:
+            while i < var:
+                self.data.append(0.0 + i)
+                i += 1
+        elif isinstance(var, tuple) == 1:
+            if len(var) != 2:
+                print("Wrong parameter, should be two int")
+            elif isinstance(var[0], int) == 0 or isinstance(var[1], int) == 0:
+                print("Wrong parameter, they should both be int")
+            elif var[1] < var [0]:
+                print("2nd parameter should be greater than the 1st one")
+            else:
+                while i < var[1] - var[0]:
+                    self.data.append(var[0] + 0.0 + i)
+                    i += 1
+        else:
+            print("Parameter is not formatted or empty")
+            
+    def __str__(self):
+        """Print Vector Content"""
+        string = "(Vector " + str(self.data) + ")"
+        return (string)
 
+    def __add__(self, x):
+        if isinstance(x, Vector) == 1:
+            if len(self.data) != len(x.data):
+                print("Vectors are not the same size")
+            else:
+                j = 0
+                v = Vector(len(self.data))
+                for item in self.data:
+                    v.data[j] = self.data[j] + v.data[j]
+                    j += 1
+        elif isinstance(x, int) == 0:
+            print("The second param should be an int or a vector")
+        else:
+            j = 0
+            v = Vector(len(self.data))
+            for i in self.data:
+                v.data[j] = self.data[j] + x
+                j += 1
+        return(v)
+    
+    def __radd__(self, x):
+        if isinstance(x, Vector) == 1:
+            if len(self.data) != len(x.data):
+                print("Vectors are not the same size")
+            else:
+                j = 0
+                v = Vector(len(self.data))
+                for item in self.data:
+                    v.data[j] = self.data[j] + v.data[j]
+                    j += 1
+        elif isinstance(x, int) == 0:
+            print("The second param should be an int")
+        else:
+            j = 0
+            v = Vector(len(self.data))
+            for i in self.data:
+                v.data[j] = self.data[j] + x
+                j += 1
+        return(v)
+    
+    def __sub__(self, x):
+        if isinstance(x, Vector) == 1:
+            if len(self.data) != len(x.data):
+                print("Vectors are not the same size")
+            else:
+                j = 0
+                v = Vector(len(self.data))
+                for item in self.data:
+                    v.data[j] = self.data[j] - v.data[j]
+                    j += 1
+        elif isinstance(x, int) == 0:
+            print("The second param should be an int")
+        else:
+            j = 0
+            v = Vector(len(self.data))
+            for i in self.data:
+                v.data[j] = self.data[j] - x
+                j += 1
+        return(v)
+    
+    def __rsub__(self, x):
+        if isinstance(x, Vector) == 1:
+            if len(self.data) != len(x.data):
+                print("Vectors are not the same size")
+            else:
+                j = 0
+                v = Vector(len(self.data))
+                for item in self.data:
+                    v.data[j] = v.data[j] - self.data[j]
+                    j += 1
+        elif isinstance(x, int) == 0:
+            print("The second param should be an int")
+        else:
+            j = 0
+            v = Vector(len(self.data))
+            for i in self.data:
+                v.data[j] = x - self.data[j]
+                j += 1
+        return(v)
+    
+    def __truediv__(self, x):
+        if isinstance(x, int) == 0:
+            print("The second param should be an int")
+        else:
+            j = 0
+            v = Vector(len(self.data))
+            for i in self.data:
+                v.data[j] = self.data[j] / x
+                j += 1
+        return(v)
+    
+    def __rtruediv__(self, x):
+        if isinstance(x, int) == 0:
+            print("The second param should be an int")
+        else:
+            j = 0
+            v = Vector(len(self.data))
+            for i in self.data:
+                v.data[j] = self.data[j] / x
+                j += 1
+        return(v)
