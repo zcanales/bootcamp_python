c1ffdc81c3273dc59777eafa0fee4c36874d3e6d
acoudouy <acoudouy@student.42.fr>
2020-01-16 16:57:16 +0100

    ex03day03 manque greyscale

--- day03/ex03/ColorFilter.py
+++ day03/ex03/ColorFilter.py
@@ -38,10 +38,24 @@ class ColorFilter():
                         new[x][y][z] = 0
         return(new)
 
+    def celluloid(self, array):
+        new = array
+        for x in range(len(array)):
+            for y in range(len(array[0])):
+                for z in range(3):
+                    if new[x][y][z] / 64 < 1:
+                        new[x][y][z] = 0
+                    elif new[x][y][z] / 64 < 2:
+                      new[x][y][z] = 64
+                    elif new[x][y][z] / 64 < 3:
+                        new[x][y][z] = 128
+                    else:
+                        new[x][y][z] = 186
+        return(new)
 
 
 test = ImageProcessor()
-array = test.load('rick.png')
+array = test.load('koala.png')
 color = ColorFilter()
 
 #new = color.invert(array)
@@ -53,5 +67,8 @@ color = ColorFilter()
 #new = color.to_green(array)
 #test.display(new)
 
-new = color.to_red(array)
+#new = color.to_red(array)
+#test.display(new)
+
+new = color.celluloid(array)
 test.display(new)
